104d2af7aa390b1ba882bd6ecc9d88069032e824
Jumman04 <arsharifuddinjumman04@gmail.com>
2023-12-06 10:56:02 +0600

    Upgrade Gradle, Fix Bugs, ReOptimized Code, Added Documentation

--- ShttpApi/src/main/java/com/shuvo/shttp/ShttpAlllApiCall.java
+++ ShttpApi/src/main/java/com/shuvo/shttp/ShttpAlllApiCall.java
@@ -14,82 +14,96 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+/**
+ * ShttpAlllApiCall is a utility class for making HTTP requests using OkHttp library.
+ * It provides methods for both GET and POST requests with asynchronous callbacks.
+ */
 public class ShttpAlllApiCall {
+
+    // Listener for handling API responses
     static ShttpallOnResponseListener shttpallOnResponseListener;
 
-
+    /**
+     * Initiates a GET request to the specified URL.
+     *
+     * @param url The URL to make the GET request to.
+     * @param key A unique identifier for the request.
+     */
     public static void getRequest(String url, String key) {
-
-
-        //shttpOnResponseListener = (ShttpOnResponseListener) context;
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url(url).build();
 
+        // Making the asynchronous call
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                shttpallOnResponseListener.onApiResponse(null, key, 0000, true, e + "");
+                // Handling the failure and notifying the listener
+                shttpallOnResponseListener.onApiResponse(null, key, 0, true, String.valueOf(e));
                 e.printStackTrace();
-                Log.e("112233", e + "");
-
+                Log.e("ShttpAlllApiCall", String.valueOf(e));
             }
 
             @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+            public void onResponse(@NonNull Call call, @NonNull Response response) {
+                // Handling the response and notifying the listener
                 try {
-
-
-                    if (response.isSuccessful()) {
-                        shttpallOnResponseListener.onApiResponse(response.body().string(), key, response.code(), false,
-                                null);
-
-
-                    }
-
+                    if (response.isSuccessful())
+                        shttpallOnResponseListener.onApiResponse(response.body().string(), key, response.code(), false, null);
                 } catch (Exception e) {
-                    Log.e("112233", e + "");
-
+                    Log.e("ShttpAlllApiCall", String.valueOf(e));
                 }
             }
         });
     }
 
-    public static void postRequest(String url, String postNody, String key) {
-        if (postNody == null) {
-            postNody = "{}";
-        }
-        OkHttpClient client = new OkHttpClient();
-        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), postNody);
-        Request request = new Request.Builder()
+    /**
+     * Initiates a POST request to the specified URL with JSON data.
+     *
+     * @param url      The URL to make the POST request to.
+     * @param postBody JSON data to be included in the request body.
+     * @param key      A unique identifier for the request.
+     */
+    public static void postRequest(String url, String postBody, String key) {
+        if (postBody == null) postBody = "{}";
 
-                .url(url).post(requestBody).build();
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = RequestBody.create(postBody, MediaType.parse("application/json; charset=utf-8"));
+        Request request = new Request.Builder().url(url).post(requestBody).build();
+
+        // Making the asynchronous call
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                shttpallOnResponseListener.onApiResponse(null, key, 0000, true, e + "");
-
+                // Handling the failure and notifying the listener
+                shttpallOnResponseListener.onApiResponse(null, key, 0, true, String.valueOf(e));
             }
 
             @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+            public void onResponse(@NonNull Call call, @NonNull Response response) {
+                // Handling the response and notifying the listener
                 try {
-                    if (response.isSuccessful()) {
+                    if (response.isSuccessful())
                         shttpallOnResponseListener.onApiResponse(response.body().string(), key, response.code(), false, null);
-
-                    }
-
-
                 } catch (Exception e) {
-
-
+                    Log.e("ShttpAlllApiCall", e.toString());
                 }
             }
         });
-
     }
 
+    /**
+     * Interface for handling API responses.
+     */
     public interface ShttpallOnResponseListener {
-        void onApiResponse(String result, String key, int statusCpde, Boolean error, String errorMessage);
+        /**
+         * Callback method invoked when an API response is received.
+         *
+         * @param result       The result or response body of the API request.
+         * @param key          A unique identifier for the request.
+         * @param statusCode   The HTTP status code indicating the success of the request.
+         * @param error        True if there is an error, false otherwise.
+         * @param errorMessage The error message describing the cause of the failure (if any).
+         */
+        void onApiResponse(String result, String key, int statusCode, boolean error, String errorMessage);
     }
-
 }
--- app/src/main/java/com/shuvo/shttpapp/MainActivity.java
+++ app/src/main/java/com/shuvo/shttpapp/MainActivity.java
@@ -6,51 +6,66 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.shuvo.shttp.OnApiresposne;
+import com.shuvo.shttp.OnApiResponse;
 import com.shuvo.shttp.ShttpApiCall;
 
+/**
+ * MainActivity represents the main activity of the application.
+ * It demonstrates the usage of the ShttpApiCall class to make GET and POST requests.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Finding UI elements
         Button getRequestBtn = findViewById(R.id.get);
         TextView tv = findViewById(R.id.textView);
-        Button postRequestBtm = findViewById(R.id.post);
+        Button postRequestBtn = findViewById(R.id.post);
+
+        // Creating an instance of ShttpApiCall
         ShttpApiCall shttpApiCall = new ShttpApiCall();
-        getRequestBtn.setOnClickListener(view -> shttpApiCall.getRequest("https://jsonplaceholder.typicode.com/posts/1", new OnApiresposne() {
+
+        // Setting up a click listener for the GET request button
+        getRequestBtn.setOnClickListener(view -> shttpApiCall.getRequest("https://jsonplaceholder.typicode.com/posts/1", new OnApiResponse() {
             @Override
-            public void onSuccessResponse(String result, int statuscode) {
+            public void onSuccessResponse(String result, int statusCode) {
+                // Updating the TextView with the successful response
                 tv.setText(result);
             }
 
             @Override
             public void onFailure(String errorMessage) {
+                // Updating the TextView with the failure message
                 tv.setText(errorMessage);
-
             }
 
             @Override
-            public void onErrorResponse(String errorMessage, int StatusCode) {
+            public void onErrorResponse(String errorMessage, int statusCode) {
+                // Updating the TextView with the error response
                 tv.setText(errorMessage);
             }
         }));
 
-        postRequestBtm.setOnClickListener(view -> shttpApiCall.postApiRequest("https://jsonplaceholder.typicode.com/posts/", new OnApiresposne() {
+        // Setting up a click listener for the POST request button
+        postRequestBtn.setOnClickListener(view -> shttpApiCall.postApiRequest("https://jsonplaceholder.typicode.com/posts/", new OnApiResponse() {
             @Override
-            public void onSuccessResponse(String result, int statuscode) {
+            public void onSuccessResponse(String result, int statusCode) {
+                // Updating the TextView with the successful response
                 tv.setText(result);
             }
 
             @Override
             public void onFailure(String errorMessage) {
+                // Updating the TextView with the failure message
                 tv.setText(errorMessage);
-
             }
 
             @Override
-            public void onErrorResponse(String errorMessage, int StatusCode) {
+            public void onErrorResponse(String errorMessage, int statusCode) {
+                // Updating the TextView with the error response
                 tv.setText(errorMessage);
             }
         }));
